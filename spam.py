aae08909bafa21e7274edc51992c5ab8c2a35ccf
Vukasin Gostovic <vukasin@gostovic.me>
2023-08-28 14:54:34 +0200

    make spam script more robust. stress test ram usage and make it infinite

--- spam.py
+++ spam.py
@@ -2,27 +2,31 @@ import multiprocessing as mp
 import requests
 import json
 
-def get_block_number():
+def get_block_number(hex_num):
     payload = {
         "jsonrpc": "2.0",
         "method": "eth_getBlockByNumber",
-        "params": ["0x1113924", False],
+        "params": [hex_num, False],
         "id": 1,
     }
     response = requests.post("http://127.0.0.1:3000", json=payload)
     result = response.json()
-    block_number = int(result["result"]["number"], 16)
+    try:
+        block_number = int(result["result"]["number"], 16)
+    except Exception as e:
+        return e
     # block_number = result
     return block_number
 
 def my_func(x):
     for i in range(x): 
-        block_number = get_block_number()
+        block_number = get_block_number(hex(i+17905056))
         print(f"Block Number: {block_number}")
 
 def main():
     pool = mp.Pool(mp.cpu_count())
-    pool.map(my_func, range(0, 1000))
+    while True:
+        pool.map(my_func, range(0, 1000))
 
 if __name__ == "__main__":
     main()
