e91680e3a289f9028958021dad34b10561bcda95
Vukasin Gostovic <vukasin@gostovic.me>
2023-08-28 13:37:14 +0200

    add macro to build cache rules

--- src/balancer/selection/cache_rules.rs
+++ src/balancer/selection/cache_rules.rs
@@ -1,5 +1,20 @@
 use memchr::memmem;
 
+// Macro for building the blacklist
+//
+// For custom blacklist rules, please read the docs and code comments
+macro_rules! blacklist {
+    () => {
+        // The blacklist contains keywords that are included in responses we dont want to cache.
+        // These responses are either to get the current state of the chain at the head or errors.
+        //
+        // For max performance, blacklist keywords should be sorted in the order of how common they are.
+        
+        
+        ["latest", "blockNumber", "error", "missing"]
+    };
+}
+
 // The default rust string contains does not use SIMD extensions
 // memchr::memmem is way faster because it uses them
 pub fn cache_method(rx: &str) -> bool {
@@ -7,7 +22,7 @@ pub fn cache_method(rx: &str) -> bool {
     #[cfg(feature = "no-cache")]
     return false;
 
-    let blacklist = ["latest", "blockNumber", "error", "missing"];
+    let blacklist = blacklist!();
 
     for item in blacklist.iter() {
         if memmem::find(rx.as_bytes(), item.as_bytes()).is_some() {
