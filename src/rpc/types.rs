e74e42826fdca4d6c01119aa5418cf21d150deb0
Vukasin Gostovic <vukasin@gostovic.me>
2023-09-14 20:11:28 +0200

    whoops

--- src/rpc/types.rs
+++ src/rpc/types.rs
@@ -83,8 +83,8 @@ impl Rpc {
     // TODO: make this work
     pub async fn get_finalized_block(&self) -> Result<u64, crate::rpc::types::RpcError> {
         let request = json!({
-            "method": "eth_getBlock".to_string(),
-            "params": "finalized".to_string(),
+            "method": "eth_getBlockByNumber".to_string(),
+            "params": ["finalized", false],
             "id": 1,
             "jsonrpc": "2.0".to_string(),
         });
