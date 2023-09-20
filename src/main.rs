9d3a20c26e32b9a3e1124a648265a813b48d92c8
Vukasin Gostovic <vukasin@gostovic.me>
2023-09-20 16:27:55 +0200

    health check toggle

--- src/main.rs
+++ src/main.rs
@@ -91,17 +91,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rpc_list_health = Arc::clone(&rpc_list_rwlock);
     let rpc_poverty_list = Arc::new(RwLock::new(Vec::<Rpc>::new()));
     let finalized = Arc::new(RwLock::new(0));
-
-    tokio::task::spawn(async move {
-        let _ = health_check(
-            rpc_list_health,
-            rpc_poverty_list,
-            finalized,
-            config.ttl,
-            config.health_check_ttl,
-        )
-        .await;
-    });
+    
+    if config.health_check {
+        tokio::task::spawn(async move {
+            let _ = health_check(
+                rpc_list_health,
+                rpc_poverty_list,
+                finalized,
+                config.ttl,
+                config.health_check_ttl,
+            )
+            .await;
+        });
+    }
 
     // We start a loop to continuously accept incoming connections
     loop {
