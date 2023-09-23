82813132bb7f122344143fa27e815c277b9f7e25
Vukasin Gostovic <vukasin@gostovic.me>
2023-09-23 18:24:55 +0200

    fix safe block

--- src/health/safe_block.rs
+++ src/health/safe_block.rs
@@ -7,7 +7,7 @@ use std::sync::{
     RwLock,
 };
 use tokio::{
-    select,
+    sync::mpsc,
     time::{
         timeout,
         Duration,
@@ -22,50 +22,62 @@ pub async fn get_safe_block(
 ) -> Result<u64, RpcError> {
     let len = rpc_list.read().unwrap().len();
     let mut safe = 0;
-    let mut threads = Vec::new();
+
+    // If len == 0 return 0
+    if len == 0 {
+        return Ok(safe);
+    }
 
     // Create a vector to store the futures of all RPC requests
     let mut rpc_futures = Vec::new();
 
+    // Create a channel for collecting results
+    let (tx, mut rx) = mpsc::channel(len);
+
     // Iterate over all RPCs
     for i in 0..len {
         let rpc_clone = rpc_list.read().unwrap()[i].clone();
+        let tx = tx.clone(); // Clone the sender for this RPC
 
         // Spawn a future for each RPC
         let rpc_future = async move {
             let a = rpc_clone.get_finalized_block();
             let result = timeout(Duration::from_millis(ttl.try_into().unwrap()), a).await;
 
-            match result {
-                Ok(response) => response.unwrap_or(0), // Handle timeout as 0
+            let reported_finalized = match result {
+                Ok(response) => response.unwrap(), // Handle timeout as 0
                 Err(_) => 0,                           // Handle timeout as 0
-            }
+            };
+
+            // Send the result to the main thread through the channel
+            tx.send(reported_finalized)
+                .await
+                .expect("head check: Channel send error");
         };
 
         rpc_futures.push(rpc_future);
     }
 
-    // Wait for all RPC futures concurrently and collect their results
+    // Wait for all RPC futures concurrently
     for rpc_future in rpc_futures {
-        let result = tokio::spawn(rpc_future);
-        threads.push(result);
+        tokio::spawn(rpc_future);
     }
 
-    // Collect the results using tokio::select!
+    // Collect the results in order from the channel
     for _ in 0..len {
-        select! {
-            result = threads.pop().unwrap() => {
-                if result.as_ref().unwrap() > &safe {
-                    safe = result.unwrap();
-                }
+        if let Some(result) = rx.recv().await {
+            if result > safe {
+                safe = result;
             }
         }
     }
 
-    // Update the finalized block
+    // Update the latest finalized block
     if safe > *finalized.read().unwrap() {
         *finalized.write().unwrap() = safe;
     }
+
+    // println!("Safe block: {}", safe);
 
     Ok(safe)
 }
