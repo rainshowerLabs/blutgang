e8b4e95234f721a3769268ae4551b662ea39e403
Vukasin Gostovic <vukasin@gostovic.me>
2023-10-14 18:01:57 +0200

    borrowing inside closure fix

--- src/balancer/selection/index_worker.rs
+++ src/balancer/selection/index_worker.rs
@@ -27,13 +27,16 @@ pub fn pick_index(
     loop {
         let _ = sleep(selection_ttl);
 
-        let rpc_list_rwlock_guard = rpc_list_rwlock.write().unwrap();
+        let mut rpc_list_rwlock_guard = rpc_list_rwlock.write().unwrap();
 
         // Sort the rpc_list fromt the fastest to the least fast RPC.
         // TODO: WE REALlY DO NOT NEED TO SORT THIS!!!!! We're wasting cpu time on useless heap operations. Just find the 2 fastest and return their indexes.
         let index = pick(&mut rpc_list_rwlock_guard);
 
         // Send new index if modified
+        let len_clone = len.clone();
+        len = rpc_list_rwlock_guard.len();
+        
         let send_if_changed = |ch_index: &mut Option<usize>| {
             if ch_index != &index {
                 *ch_index = index;
@@ -41,7 +44,7 @@ pub fn pick_index(
             }
 
             // The index might be the same but the rpc in the index might have changed
-            if len != rpc_list_rwlock_guard.len() {
+            if len_clone != rpc_list_rwlock_guard.len() {
                 // *ch_index = index; // prob dont need this since index !changed?
                 return true;
             }
@@ -49,7 +52,6 @@ pub fn pick_index(
             false
         };
 
-        len = rpc_list_rwlock_guard.len();
 
         rpc_index_tx.send_if_modified(send_if_changed);
     }
