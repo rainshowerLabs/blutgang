1759ae4e4b68c482214ec2d443cc21e5c8000b4c
Vukasin Gostovic <vukasin@gostovic.me>
2023-10-28 19:26:31 +0200

    comment out the blutgang_quit check

--- src/balancer/balancer.rs
+++ src/balancer/balancer.rs
@@ -118,10 +118,10 @@ macro_rules! get_response {
 
                     // Quit blutgang if `tx_string` contains the word `blutgang_quit`
                     // Only for debugging, remove this for production builds *manually*.
-                    if memmem::find(&tx_string.as_bytes(), "blutgang_".as_bytes()).is_some() {
-                        let _ = $cache.flush_async().await;
-                        std::process::exit(0);
-                    }
+                    // if memmem::find(&tx_string.as_bytes(), "blutgang_".as_bytes()).is_some() {
+                    //     let _ = $cache.flush_async().await;
+                    //     std::process::exit(0);
+                    // }
 
                     // Loop until we get a response
                     let rx;
