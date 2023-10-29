9666b30df63b7a22cd01aa0f637dcbdb928f1f87
Vukasin Gostovic <vukasin@gostovic.me>
2023-10-29 13:26:00 +0100

    print misconfiguration warnings based on the db hash

--- src/config/cache_setup.rs
+++ src/config/cache_setup.rs
@@ -12,4 +12,23 @@ pub fn setup_data(cache: Arc<Db>) {
         "{\"jsonrpc\":\"2.0\",\"id\":null,\"result\":\"blutgang v0.2.0 nc \
         Dedicated to the spirit that lives inside of the computer\"}",
     );
+
+    // Insert which hashing algo we're using based on the selected features.
+    // If `xxhash` is enabled we're using xxhash3, otherwise blake3.
+    //
+    // Print a warning if we see an keys are in an unexpectd hash format.
+    if cfg!(feature = "xxhash") {
+        let _ = cache.insert(b"xxhash", b"true");
+        if cache.get(b"blake3").unwrap().is_some() {
+            println!("\x1b[93mWrn:\x1b[0m Blutgang has detected that your DB is using blake3 while we're currently using xxhash! \
+                Please remove all cache entries and try again.");
+        }
+    } else {
+        let _ = cache.insert(b"blake3", b"true");
+        if cache.get(b"xxhash").unwrap().is_some() {
+            println!("\x1b[93mWrn:\x1b[0m Blutgang has detected that your DB is using xxhash while we're currently using blake3! \
+                Please remove all cache entries and try again.");
+        }
+    }
+
 }
