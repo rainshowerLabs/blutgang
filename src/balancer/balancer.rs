f293dbf583fa9ffb997fd69251bef6d0f1e69f59
Vukasin Gostovic <vukasin@gostovic.me>
2023-10-31 16:46:29 +0100

    verified finalized_rx works as intended

--- src/balancer/balancer.rs
+++ src/balancer/balancer.rs
@@ -182,9 +182,10 @@ macro_rules! get_response {
                         let num = get_block_number_from_request($tx, $named_numbers);
                         if num.is_some() {
                             let num = num.unwrap();
+                                println!("finalized_rx: {}", *$finalized_rx.borrow());
 
                             if num > *$finalized_rx.borrow() {
-                                // TODO: test this
+                                println!("finalized_rx (real): {}", *$finalized_rx.borrow());
                                 let mut head_cache = $head_cache.write().unwrap();
                                 head_cache
                                     .entry(num)
