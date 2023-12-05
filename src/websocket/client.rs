f9caa7544f6ce9081e57a225075e3c08186d6f69
Vukasin Gostovic <vukasin@gostovic.me>
2023-12-05 12:30:43 +0100

    bigmanting

--- src/websocket/client.rs
+++ src/websocket/client.rs
@@ -1,6 +1,20 @@
-use std::format;
+use crate::Rpc;
 
+use std::{
+    format,
+    sync::{
+        RwLock,
+        Arc,
+    },
+};
 use async_trait::async_trait;
+
+use tokio::{
+    sync::{
+        watch,
+        mpsc,
+    },
+};
 
 pub struct Client {
     pub handle: ezsockets::Client<Self>,
@@ -27,12 +41,29 @@ impl ezsockets::ClientExt for Client {
     }
 }
 
-// Open
-// fn ws_conn_manager(
-//     rpc_list: Arc<RwLock<Vec<Rpc>>>,
-// ) -> () {
+// Open WS connections to our nodes and accept and process internal WS calls
+// whenever we receive something from incoming_rx
+fn ws_conn_manager(
+    rpc_list: Arc<RwLock<Vec<Rpc>>>,
+    incoming_rx: mpsc::UnboundedReceiver<String>,
+    outgoing_tx: watch::Sender<String>,
+) -> () {
+    let rpc_list_clone = rpc_list.read().unwrap().clone();
 
-// }
+
+    let mut ws_handles = Vec::new();
+    for rpc in rpc_list_clone {
+        let config = ClientConfig::new(rpc.ws_url);
+        let (handle, future) = ezsockets::connect(|handle| Client { handle }, config).await;        
+        tokio::spawn(async move {
+            future.await.unwrap();
+        });
+        ws_handles.push(handle);
+    }
+
+
+
+}
 
 // Receive JSON-RPC call from balancer thread and respond with ws response
 pub async fn execute_ws_call(call: String) -> Result<String, ezsockets::Error> {
