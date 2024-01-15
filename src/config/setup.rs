196189d0ba460d604ff70e3257e47c85ca1b5e69
Vukasin Gostovic <vukasin@gostovic.me>
2024-01-15 18:29:16 +0100

    version bump

--- src/config/setup.rs
+++ src/config/setup.rs
@@ -7,7 +7,7 @@ pub const WS_HEALTH_CHECK_USER_ID: u32 = 1;
 pub const WS_SUB_MANAGER_ID: u32 = 2;
 
 // Version consts, dont impact functionality
-pub const VERSION_STR: &str = "blutgang 0.3.0-canary2 Garreg Mach";
+pub const VERSION_STR: &str = "blutgang 0.3.0-rc1 Garreg Mach";
 pub const TAGLINE: &str = "`Now there's a way forward.`";
 
 // Do `ma_length`amount eth_blockNumber calls per rpc and then sort them by latency
