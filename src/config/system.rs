11efe4fcc920ef216be4fa4bf8b98d8a1d4c6910
nuts-rice <red44443@gmail.com>
2024-03-07 17:12:55 -0500

    🚧 prometheus metrics

--- src/config/system.rs
+++ src/config/system.rs
@@ -18,15 +18,89 @@ pub fn log_statsd(tags: &[str], message: &str) {
     unimplemented!()
 }
 
+// #[cfg(feature = "prometheusd")]
+// pub fn log_prometheus(metric: &str) {
+//     use metrics_prometheus::Recorder;
+//     use prometheus::Gauge;
+//     let _metric = prometheus::Gauge::new(format!("{metric}"), "help").unwrap();
+//     let recorder = Recorder::builder()
+//         .with_metric(_metric.clone())
+//         .build_and_install();
+// }
+
 #[cfg(feature = "prometheusd")]
-pub fn log_prometheus(metric: &str) {
-    use metrics_prometheus::Recorder;
-    use prometheus::Gauge;
-    let _metric = prometheus::Gauge::new(format!("{metric}"), "help").unwrap();
-    let recorder = Recorder::builder()
-        .with_metric(_metric.clone())
-        .build_and_install();
+pub fn gather_metrics() -> Result<String, prometheus::Error> {
+    use crate::log_info;
+    use prometheus::gather;
+    let report = prometheus::TextEncoder::new()
+        .encode_to_string(&prometheus::default_registry().gather())?
+        .trim()
+        .to_string();
+    log_info!("Prometheus metrics: {}", report);
+    Ok(report)
+    // gather::gather(&prometheus::gather(), &mut buffer).unwrap();
+    // let output = String::from_utf8(buffer).unwrap();
+    // println!("{}", output);
 }
+
+// #[cfg(feature = "prometheusd")]
+// use crate::config::error::ConfigError;
+// use prometheus::{proto::{Metric, MetricType, MetricFamily, LabelPair}, Encoder, Result};
+// use std::{collections::HashMap, io::Write};
+#[cfg(feature = "prometheusd")]
+pub fn encode_metrics() {
+    let report = gather_metrics();
+
+    // let mut buffer = vec![];
+    // encoder.encode(&metric_families, &mut buffer).unwrap();
+    // let output = String::from_utf8(buffer).unwrap();
+}
+
+// #[cfg(feature = "prometheusd")]
+// #[derive(Debug, Default)]
+// pub struct JsonEncoder;
+// #[cfg(feature = "prometheusd")]
+// impl Encoder for JsonEncoder {
+//     fn encode<W: Write>(&self, metric_families: &[MetricFamily], writer: &mut W) -> Result<()> {
+//         let mut encoded : HashMap<String, f64> = HashMap::new();
+//         for metric_family in metric_families {
+//             let name = metric_family.get_name();
+//             let metric_type = metric_family.get_field_type();
+//             for metric in metric_family.get_metric() {
+//                 match metric_type {
+//                     MetricType::COUNTER => {
+//                 encoded.entry(metric).and_modify;
+//                 metric.get_counter().get_value();
+
+//                     },
+//                     MetricType::GAUGE => {
+//                 encoded.entry(name.to_string(), metric);
+//                 metric.get_gauge().get_value();
+
+//                     },
+//                     MetricType::HISTOGRAM => {
+//                         let histogram = metric.get_histogram();
+//                         encoded.insert(name.to_string(), histogram.get_sample_sum());
+//                     },
+//                     _ => {
+//                         eprintln!("Unsupported metric type: {:?}", metric_type);
+//                     },
+//                 }
+//             }
+//         }
+//         match serde_json::to_string(&encoded) {
+//             Ok(_) => {
+//                 writer.write_all(encoded.as_bytes())?;
+//             },
+//             Err(e) => eprintln!("Failed to encode metric as JSON! Error : {}", e.to_string())
+//         }
+//         Ok(())
+//     }
+
+//     fn format_type(&self) -> &str {
+//         "json"
+//     }
+// }
 
 #[cfg(feature = "dogstatd")]
 pub fn log_dogstatsd(tags: &[str], message: &str) {
