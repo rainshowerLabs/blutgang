9af405ef9174790f45b9417567319e0ed0a5ec2b
nuts-rice <red44443@gmail.com>
2024-03-11 10:33:29 -0400

    🔊 prometheus logging latency , debug print of bucket

--- src/config/system.rs
+++ src/config/system.rs
@@ -13,32 +13,44 @@ use prometheus_metric_storage::{MetricStorage, StorageRegistry};
 use std::time::Duration;
 
 //Some goofy Rust stuff
-#[cfg(feature = "prometheusd")]
+// #[cfg(feature = "prometheusd")]
 static METRICS_REGISTRY: Lazy<StorageRegistry> = Lazy::new(|| {
     let registry = Registry::new_custom(Some("blutgang".to_string()), None).unwrap();
     StorageRegistry::new(registry)
 });
-#[cfg(feature = "prometheusd")]
+// #[cfg(feature = "prometheusd")]
 #[derive(MetricStorage, Clone, Debug)]
 #[metric(subsystem = "rpc")]
 pub struct RpcMetrics {
     #[metric(labels("path", "method", "status"), help = "Total number of requests")]
-    requests_complete: prometheus::IntCounterVec,
-    #[metric(labels("path", "method"), help = "Duration of requests")]
+    requests: prometheus::IntCounterVec,
+    #[metric(labels("path", "method"), help = "latency of rpc calls")]
     duration: prometheus::HistogramVec,
 }
-#[cfg(feature = "prometheusd")]
+// #[cfg(feature = "prometheusd")]
 impl RpcMetrics {
     pub fn inst(registry: &StorageRegistry) -> Result<&Self, prometheus::Error> {
         RpcMetrics::instance(registry)
     }
     pub fn requests_complete(&self, path: &str, method: &str, status: &u16, duration: Duration) {
-        let dt = (duration.as_nanos() as f64) / 1_000_000_000.0;
-        self.requests_complete
+        let dt = duration.as_millis() as f64;
+        self.requests
             .with_label_values(&[path, method, &status.to_string()])
             .inc();
         self.duration.with_label_values(&[path, method]).observe(dt)
     }
+    pub fn push_latency(&self, path: &str, method: &str, dt: f64) {
+        self.duration.with_label_values(&[path, method]).observe(dt)
+    }
+}
+
+//  #[cfg(feature = "prometheusd")]
+pub fn encode(registry: &prometheus::Registry) -> String {
+    use prometheus::Encoder;
+    let encoder = prometheus::TextEncoder::new();
+    let mut buffer = vec![];
+    encoder.encode(&registry.gather(), &mut buffer).unwrap();
+    String::from_utf8(buffer).unwrap()
 }
 
 #[cfg(feature = "journald")]
@@ -47,58 +59,13 @@ pub fn log_journald(level: u32, message: &str) {
     journal::print(level, message);
 }
 
-#[cfg(feature = "statsd")]
-pub fn log_statsd(tags: &[str], message: &str) {
-    unimplemented!()
-}
-
-#[cfg(feature = "prometheusd")]
+// #[cfg(feature = "prometheusd")]
 pub fn get_storage_registry() -> &'static StorageRegistry {
     &METRICS_REGISTRY
 }
-
-#[cfg(feature = "prometheusd")]
+// #[cfg(feature = "prometheusd")]
 pub fn get_registry() -> &'static Registry {
     get_storage_registry().registry()
-}
-
-#[cfg(feature = "prometheusd")]
-pub fn recorder_init() -> Result<metrics_prometheus::Recorder, prometheus::Error> {
-    use metrics_prometheus::*;
-    let recorder = metrics_prometheus::install();
-    Ok(recorder)
-}
-
-#[cfg(feature = "prometheusd")]
-pub fn gather_metrics() -> Result<serde_json::Value, serde_json::Error> {
-    use crate::log_info;
-    use metrics_prometheus::*;
-    use prometheus::gather;
-    use prometheus::Gauge;
-    use serde_json::*;
-    let recorder = recorder_init().unwrap();
-    let report = prometheus::TextEncoder::new().encode_to_string(&recorder.registry().gather());
-    let result = serde_json::Value::String(report.unwrap());
-    log_info!("Prometheus metrics: {:?}", result);
-    Ok(result)
-}
-
-//TODO: json format
-#[cfg(feature = "prometheusd")]
-pub fn format_metrics() {
-    let report = gather_metrics().unwrap();
-    //    let type = report.get("TYPE").unwrap();
-}
-
-#[cfg(feature = "prometheusd")]
-pub fn encode_metrics() {
-    let report = gather_metrics();
-}
-
-#[cfg(feature = "dogstatd")]
-pub fn log_dogstatsd(tags: &[str], message: &str) {
-    use dogstatsd::{Client, Options, OptionsBuilder};
-    let client = Client::new(Options::default()).unwrap();
 }
 
 #[macro_export]
@@ -165,89 +132,37 @@ macro_rules! log_err {
 }
 
 #[macro_export]
-macro_rules! dogstatd_latency {
-    () => {};
-}
-#[macro_export]
-macro_rules! dogstat_info {
-    ($fmt:expr, $($arg:tt)*) => {
-        let message = format!($fmt, $($arg)*);
-        #[cfg(feature = "dogstatd")]
+macro_rules! prometheusd_latency {
+        ($fmt:expr, $($arg:tt)*) => {
+        let rpc_path = format!($fmt, $($arg)*);
+        // #[cfg(feature = "prometheusd")]
         {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(6, &message);
+            use $crate::config::system::RpcMetrics;
+            use $crate::config::system::get_storage_registry;
+            let registry = get_storage_registry();
+            let metric = RpcMetrics::inst(registry).unwrap();
+            let start = std::time::Instant::now();
+            move |status: u16| {
+                let duration = start.elapsed();
+                metric.requests_complete(duration);
+            }
         }
-        println!("\x1b[35mInfo:\x1b[0m {}", message)
-    };
-    ($fmt:expr) => {
-        #[cfg(feature = "dogstatd")]
-        {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(6, $fmt);
+        };
 
-        }
-        println!(concat!("\x1b[35mInfo:\x1b[0m ", $fmt))
-    };
 }
-
-#[macro_export]
-macro_rules! dogstatd_wrn {
-    ($fmt:expr, $($arg:tt)*) => {
-        let message = format!($fmt, $($arg)*);
-        #[cfg(feature = "dogstatd")]
-        {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(4, &message);
-        }
-        println!("\x1b[93mWrn:\x1b[0m {}", message)
-    };
-    ($fmt:expr) => {
-        #[cfg(feature = "dogstatd")]
-        {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(4, $fmt);
-        }
-        println!(concat!("\x1b[93mWrn:\x1b[0m ", $fmt))
-    };
-}
-
-#[macro_export]
-macro_rules! dogstatd_err {
-    ($fmt:expr, $($arg:tt)*) => {
-        let message = format!($fmt, $($arg)*);
-        #[cfg(feature = "dogstatd")]
-        {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(3, &message);
-        }
-        println!("\x1b[31mErr:\x1b[0m {}", message)
-    };
-    ($fmt:expr) => {
-        #[cfg(feature = "dogstatd")]
-        {
-            use $crate::config::system::log_dogstatsd;
-            log_dogstatsd(3, $fmt);
-        }
-        println!(concat!("\x1b[31mErr:\x1b[0m ", $fmt))
-    };
-}
-
-#[cfg(feature = "prometheusd")]
 #[cfg(test)]
 mod tests {
-    use std::any::Any;
 
     //TODO: remove this after tests
     //sorry, im too lazy to make proper tests for this
-    use super::*;
-    use prometheus::proto::Gauge;
+
     #[tokio::test]
     async fn test_prometheus_log() {
-        let registry  = get_storage_registry();
-        let metrics = RpcMetrics::inst(registry);
-
-
-//         let expected = "prometheus_metrics";
-//         assert_eq!(report, expected);
+        // let rpc1 = Rpc::default();
+        // let registry  = get_storage_registry();
+        // let metrics = RpcMetrics::inst(registry);
+        // let expected = "prometheus_metrics";
+        // assert_eq!(report, expected);
+        unimplemented!();
     }
 }
