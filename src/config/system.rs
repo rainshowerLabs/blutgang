439b7186cdc235ebb2e01a6ec8bc2dcbb9a536c4
Vukasin Gostovic <vukasin@gostovic.me>
2024-03-15 19:44:01 +0100

    bump version

--- src/config/system.rs
+++ src/config/system.rs
@@ -4,7 +4,7 @@ pub const WS_SUB_MANAGER_ID: u32 = 2;
 pub const MAGIC: u32 = 0xb153;
 
 // Version consts, dont impact functionality
-pub const VERSION_STR: &str = "Blutgang 0.3.3 Garreg Mach";
+pub const VERSION_STR: &str = "Blutgang 0.3.3-rc1 Garreg Mach";
 pub const TAGLINE: &str = "`Now there's a way forward.`";
 
 #[cfg(feature = "journald")]
