b3c567f9a311871c0f5a59ff04624fe73eb93af5
nuts-rice <red44443@gmail.com>
2024-07-25 14:25:41 -0400

    🔊 docker container for prometheus

--- src/admin/metrics.rs
+++ src/admin/metrics.rs
@@ -1,8 +1,11 @@
 use crate::Rpc;
 use hex::encode;
 use http_body_util::Full;
-use hyper::body;
 use hyper::server::conn::http1;
+use hyper::{
+    body,
+    header,
+};
 use hyper::{
     body::Bytes,
     body::Incoming,
@@ -23,6 +26,7 @@ use prometheus_metric_storage::{
     MetricStorage,
     StorageRegistry,
 };
+
 use serde_json::{
     json,
     Value,
@@ -65,50 +69,6 @@ pub type MetricUpdateSender = tokio::sync::mpsc::Sender<MetricsUpdate>;
 pub type MetricUpdateReciever = tokio::sync::mpsc::Receiver<MetricsUpdate>;
 
 const VERSION_LABEL: [(&str, &str); 1] = [("version", env!("CARGO_PKG_VERSION"))];
-
-// #[cfg(feature = "prometheusd")]
-// pub(crate) async fn metrics_service(
-//     registry_rwlock: Arc<RwLock<StorageRegistry>>,
-//     config_rwlock: Arc<RwLock<Settings>>,
-// ) {
-//     use crate::log_info;
-//     use hyper_util_blutgang::rt::TokioIo;
-//     use tokio::{
-//         net::TcpListener,
-//         sync::mpsc,
-//     };
-
-//     let (metrics_request_tx, metrics_request_rx) = metrics_update_channel().await;
-//     let (metrics_tx, metrics_rx) = metrics_channel().await;
-//     let (addr, update_interval) = {
-//         let config_guard = config_rwlock.read().unwrap();
-//         (
-//             config_guard.metrics.address,
-//             config_guard.metrics.count_update_interval,
-//         )
-//     };
-//     let registry_guard = registry_rwlock.read().unwrap();
-//     let metrics = RpcMetrics::new("metrics_service").to_owned();
-//     let metrics_rwlock = Arc::new(RwLock::new(metrics));
-//     let encoder_rwlock = Arc::new(RwLock::new(BufferedTextEncoder::new()));
-//     let service = PrometheusHandle::new(
-//         encoder_rwlock,
-//         metrics_rwlock,
-//         registry_rwlock.clone(),
-//         config_rwlock,
-//     );
-//     let listener = TcpListener::bind(addr).await;
-
-//     log_info!("Bound metrics to : {}", addr);
-//     let mut interval = tokio::time::interval(Duration::from_secs(update_interval));
-//     let (stream) = listener.unwrap().accept().await;
-//     let io = TokioIo::new(stream.unwrap());
-//     // http1::Builder::new()
-//     //     .serve_connection(io , service_fn(|req| {
-//     //         let response = metrics_handler(service.clone());
-//     //         response
-//     //                 });
-// }
 
 #[derive(Clone, Debug)]
 pub struct RpcMetrics {
@@ -180,155 +140,6 @@ pub async fn metrics_update_sink(mut metrics_rx: MetricUpdateReciever) {
     }
 }
 
-// #[cfg(feature = "prometheusd")]
-// pub async fn listen_for_metrics_requests(
-//     config: Arc<RwLock<Settings>>,
-//     rpc_list: Arc<RwLock<Vec<Rpc>>>,
-// ) -> Result<(), Box<dyn std::error::Error>> {
-//     use crate::config::{
-//         cache_setup::setup_data,
-//         cli_args::create_match,
-//     };
-//     use crate::log_info;
-//     let (address, interval) = {
-//         let config_guard = config.read().unwrap();
-//         (
-//             config_guard.metrics.address,
-//             config_guard.metrics.count_update_interval,
-//         )
-//     };
-
-//     let (metrics_request_tx, metrics_request_rx) = metrics_update_channel().await;
-//     let (metrics_tx, metrics_rx) = metrics_channel().await;
-//     let metrics_request_tx_rwlock = Arc::new(RwLock::new(metrics_request_tx.clone()));
-//     let metrics_tx_rwlock = Arc::new(RwLock::new(metrics_tx.clone()));
-//     tokio::spawn(metrics_monitor(
-//         metrics_request_rx,
-//         // registry_status.clone(),
-//         rpc_list.clone(),
-//         rpc_position,
-//         dt,
-//     ));
-//     metrics_server(
-//         metrics_tx_rwlock,
-//         // registry_status.clone(),
-//         metrics_tx,
-//         address,
-//         interval,
-//     )
-//     .await;
-//     let metrics_report = forward_metrics(registry_status).await;
-//     log_info!("metrics response: {:?}", metrics_report);
-//     Ok(())
-// }
-//#[cfg(feature = "prometheusd")]
-//pub async fn metrics_listener(
-//    mut metrics_rx: MetricUpdateReciever,
-//    metrics_status: Arc<RwLock<RpcMetrics>>,
-//    rpc_list: &Arc<RwLock<Vec<Rpc>>>,
-//    position: usize,
-//    dt: Duration,
-//) {
-//    while let Some(update) = metrics_rx.recv().await {
-//        match update {
-//            MetricsUpdate::Http => {
-//                let mut metrics_guard = metrics_status.write().unwrap();
-//                let rpc_list_guard = rpc_list.read().unwrap_or_else(|e| e.into_inner());
-//                metrics_guard.requests_complete(
-//                    &rpc_list_guard[position].name,
-//                    &"http",
-//                    &"200",
-//                    dt,
-//                );
-//            }
-//            MetricsUpdate::Websocket => {
-//                let mut metrics_guard = metrics_status.write().unwrap();
-//                let rpc_list_guard = rpc_list.read().unwrap_or_else(|e| e.into_inner());
-//                metrics_guard.requests_complete(
-//                    &rpc_list_guard[position].name,
-//                    &"WebSocket",
-//                    &"200",
-//                    dt,
-//                );
-//            }
-//            MetricsUpdate::Database => {
-//                let mut metrics_guard = metrics_status.write().unwrap();
-//                let rpc_list_guard = rpc_list.read().unwrap_or_else(|e| e.into_inner());
-//                metrics_guard.requests_complete(
-//                    &rpc_list_guard[position].name,
-//                    &"Database",
-//                    &"200",
-//                    dt,
-//                );
-//            }
-//        }
-//    }
-//}
-///// Matches for command, accepts metrics request, encodes and prints
-//#[cfg(feature = "prometheusd")]
-///// Accepts metrics request, encodes and prints
-//#[cfg(feature = "prometheusd")]
-//pub async fn write_metrics_response(
-//    tx: Request<hyper::body::Incoming>,
-//    metrics_tx: Arc<RwLock<MetricSender>>,
-//    // registry_state: Arc<RwLock<StorageRegistry>>,
-//) -> Result<hyper::Response<Full<Bytes>>, Infallible> {
-//    use crate::{
-//        balancer::format::incoming_to_value,
-//        log_info,
-//    };
-//    use serde_json::{
-//        json,
-//        Value,
-//        Value::Null,
-//    };
-
-//    let metrics_report = metrics_encoder(registry_state).await;
-//    let response = Ok(hyper::Response::builder()
-//        .status(200)
-//        .body(Full::from(Bytes::from(metrics_report)))
-//        .unwrap());
-//    log_info!("metrics response: {:?}", response);
-//    (response)
-//}
-//#[cfg(feature = "prometheusd")]
-//async fn metrics_encoder() -> String {
-//    use prometheus::Encoder;
-//    let encoder = prometheus::TextEncoder::new();
-//    let mut buffer = vec![];
-//    let registry = prometheus::gather();
-//    encoder.encode(&registry, &mut buffer).unwrap();
-//    String::from_utf8(buffer).unwrap()
-//}
-
-////listens for updates to metrics and updates the storage registry
-//#[cfg(feature = "prometheusd")]
-//pub(in crate::r#admin) async fn metrics_monitor(
-//    metrics_rx: MetricUpdateReciever,
-//    // storage_registry: Arc<RwLock<StorageRegistry>>,
-//    rpc_list: Arc<RwLock<Vec<Rpc>>>,
-//    rpc_postion: usize,
-//    dt: Duration,
-//) {
-//    // let registry;
-//    // let registry_guard = storage_registry.read().unwrap();
-//    // registry = registry_guard;
-//    let metrics_status = Arc::new(RwLock::new(
-//        RpcMetrics::new("metrics_monitor").unwrap().to_owned(),
-//    ));
-//    let metrics_stat_listener = metrics_status.clone();
-//    let rpc_list_clone = Arc::clone(&rpc_list);
-//    tokio::spawn(async move {
-//        let _ = metrics_listener(
-//            metrics_rx,
-//            metrics_stat_listener,
-//            &rpc_list_clone,
-//            rpc_postion,
-//            dt,
-//        )
-//        .await;
-//    });
-//}
 pub async fn metrics_channel() -> (MetricSender, MetricReceiver) {
     let (tx, rx) = unbounded_channel();
     (tx, rx)
